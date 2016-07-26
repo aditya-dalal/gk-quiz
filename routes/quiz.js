57b9f7828413c8e52e281e8f7292a7500e3b4e81
Aditya Dalal <aditya.dalal@flipkart.com>
2016-07-26 18:56:08 +0530

    Added quiz logic

--- routes/quiz.js
+++ routes/quiz.js
@@ -6,12 +6,48 @@ var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
 
-var que = mongoose.model('question');
+var collection = mongoose.model('question');
+var asked = {};
+var total;
 
 router.get('/', function(req, res) {
-    que.findOne({number: 20}, function(err, doc){
-        res.render('quiz', {title: "Quiz", doc: doc});
+    collection.count(function(err, count){
+        total = count+1;
+        asked = {};
+        collection.findOne({number: getNextNumber()}, function(err, doc){
+            res.render('quiz', {title: "Quiz", doc: doc});
+        });
     });
 });
+
+router.get('/next', function(req, res) {
+    var num = getNextNumber();
+    if (num === "done") {
+        var doc = {question: "", answer: ""};
+        res.render('quiz', {title: "Quiz", doc: doc, done: "No more new questions. Go back to home page for retest."});
+    }
+    else {
+        collection.findOne({number: num}, function (err, doc) {
+            res.render('quiz', {title: "Quiz", doc: doc});
+        });
+    }
+});
+
+var getNextNumber = function(){
+    if(Object.keys(asked).length == total-1){
+        return "done";
+    }
+    while(true){
+        var num = random(1, total);
+        if(asked[num] === undefined){
+            asked[num] = true;
+            return num;
+        }
+    }
+};
+
+var random = function(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
 
 module.exports = router;
